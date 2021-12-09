682377166ebb0b8b3d86be0694f4aecaa45a2994
shuaibw <1805010@ugrad.cse.buet.ac.bd>
2021-12-09 23:54:59 +0600

    Fix starter files

--- Offline2/src/BellManFord.java
+++ Offline2/src/BellManFord.java
@@ -1,2 +1,2 @@
-package PACKAGE_NAME;public class BellManFord {
+public class BellManFord {
 }
--- Offline2/src/Dijkstra.java
+++ Offline2/src/Dijkstra.java
@@ -1,2 +1,45 @@
-package PACKAGE_NAME;public class Dijkstra {
+import java.util.Arrays;
+import java.util.Comparator;
+import java.util.PriorityQueue;
+
+public class Dijkstra {
+    private class Vertex {
+        private int v;
+        private double cost;
+
+        private Vertex(int v, double cost) {
+            this.v = v;
+            this.cost = cost;
+        }
+    }
+
+    private double[] dist;
+    private boolean[] visited;
+    private int[] prev;
+    private PriorityQueue<Vertex> pq;
+
+    public Dijkstra(WeightedDigraph g) {
+        dist = new double[g.V()];
+        visited = new boolean[g.V()];
+        prev = new int[g.V()];
+        pq = new PriorityQueue<>(Comparator.comparingDouble(o -> o.cost));
+    }
+
+    public int[] computeSSSP(int from, int to) {
+        clear();
+        dist[from] = 0;
+        pq.add(new Vertex(from, 0));
+        while(!pq.isEmpty()){
+            Vertex cur = pq.poll();
+
+        }
+    }
+
+    private void clear() {
+        Arrays.fill(visited, false);
+        Arrays.fill(dist, Double.MAX_VALUE);
+        Arrays.fill(prev, -1);
+        pq.clear();
+    }
+
 }
